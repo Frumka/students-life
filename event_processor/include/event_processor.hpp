8cdef9b9f5c828d7add9218241b647d83e4d78dc
Frumka <Hekchanel@mail.ru>
2019-12-02 04:03:39 +0300

    event processor major update 1. operation +=, -= added 2. added event saving

--- event_processor/include/event_processor.hpp
+++ event_processor/include/event_processor.hpp
@@ -8,21 +8,33 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 #include <sstream>
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
-std::vector<std::string> split(const std::string& s, char delimiter='.')
-{
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(s);
-    while (std::getline(tokenStream, token, delimiter))
-    {
-        tokens.push_back(token);
+namespace event_parser_tools {
+    bool is_inside(const std::string &main, const std::string &sub) {
+        return (main.find(sub) != std::string::npos);
     }
-    return tokens;
+
+    std::vector<std::string> split(const std::string& s, const std::string& delimiter = ".") {
+        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+        std::string token;
+        std::vector<std::string> res;
+
+        while ((pos_end = s.find (delimiter, pos_start)) != std::string::npos) {
+            token = s.substr (pos_start, pos_end - pos_start);
+            pos_start = pos_end + delim_len;
+            res.push_back (token);
+        }
+
+        res.push_back (s.substr (pos_start));
+        return res;
+    }
 }
+
+using namespace event_parser_tools;
 
 class event_processor{
 public:
@@ -40,7 +52,7 @@ public:
         acessor = std::move(map);
     }
 
-    void load_event(std::string path){
+    void load_event(const std::string& path){
         std::ifstream text_stream(path);
         text_stream >> saved_event;
     }
@@ -50,16 +62,41 @@ public:
     }
 
     void execute_string(const std::string& command){
-        auto command_parts = split(command, '=');
+        std::string operation;
 
-        auto steps = split(command_parts[0]);
-        auto result = command_parts[1];
+        if(is_inside(command, "+="))
+            operation = "+=";
+        else if(is_inside(command, "-="))
+            operation = "-=";
+        else if(is_inside(command, "="))
+            operation = "just =";
+        else
+            throw std::invalid_argument("bad string event given: '" + command + "' , no '=' sign detected");
 
-        *acessor[steps[0]][steps[1]] = result;
-    }
+        if(operation == "just =") {
+            auto command_parts = split(command, "=");
 
-    void drop_acessor(){
-        acessor = {};
+            auto steps = split(command_parts[0]);
+            auto result = command_parts[1];
+
+            *acessor[steps[0]][steps[1]] = result;
+            return;
+        }
+        else{
+            auto command_parts = split(command, operation);
+
+            auto steps = split(command_parts[0]);
+
+            double result = std::stod(*acessor[steps[0]][steps[1]]); // previous value
+
+            if(operation == "+=")
+                result += std::stod(command_parts[1]);
+            else
+                result -= std::stod(command_parts[1]);
+
+            *acessor[steps[0]][steps[1]] = std::to_string(result);
+            return;
+        }
     }
 
     void execute_button(int button_number){
--- event_processor/tests/test.cpp
+++ event_processor/tests/test.cpp
@@ -23,6 +23,31 @@ public:
         return mark;
     }
 };
+
+TEST(event_processor_tools, split){
+    std::string test0 = "hate.this.task";
+    std::vector<std::string> res0 = {"hate", "this", "task"};
+    EXPECT_EQ(event_parser_tools::split(test0), res0);
+
+    std::string test1 = "split me pls";
+    std::vector<std::string> res1 = {"split", "me", "pls"};
+    EXPECT_EQ(event_parser_tools::split(test1, " "), res1);
+
+    std::string test2 = "kill_me_pls";
+    std::vector<std::string> res2 = {"kill", "pls"};
+    EXPECT_EQ(event_parser_tools::split(test2, "_me_"), res2);
+}
+
+TEST(event_processor_tools, is_inside){
+    EXPECT_TRUE(event_parser_tools::is_inside("hereis.dot", "."));
+
+    EXPECT_FALSE(event_parser_tools::is_inside("noExclamHere... Sad -_-" , "!"));
+
+    EXPECT_TRUE(event_parser_tools::is_inside("Close += example", "+="));
+
+    EXPECT_FALSE(event_parser_tools::is_inside("Even = closer", "+="));
+}
+
 TEST(event_processor, constructor){
     prepod Aliev, Konova;
 
@@ -45,7 +70,7 @@ TEST(event_processor, constructor){
 }
 
 
-TEST(event_processor, string_execution){
+TEST(event_processor, string_execution_set){
     prepod Aliev, Konova;
 
     std::map<std::string, std::map<std::string, std::string*>> acessor = {
@@ -65,6 +90,29 @@ TEST(event_processor, string_execution){
     *acessor["Aliev"]["mark"]="2";
 
     EXPECT_EQ(Aliev.checkit(), "2");
+
+}
+
+TEST(event_processor, string_execution_add_substract){
+    prepod Aliev, Konova;
+
+    std::map<std::string, std::map<std::string, std::string*>> acessor = {
+            {"Aliev", Aliev.acessor},
+            {"Konova", Konova.acessor},
+    };
+
+    EXPECT_EQ(Aliev.checkit(), "2");
+
+    event_processor processor;
+    processor.link_acessor(acessor);
+
+    processor.execute_string("Aliev.mark+=5");
+
+    EXPECT_EQ(std::stod(Aliev.checkit()), 7);
+
+    processor.execute_string("Aliev.mark-=2");
+
+    EXPECT_EQ(std::stod(Aliev.checkit()), 5);
 
 }
 
