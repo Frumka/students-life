8131a4ebe7962fdddb89d614a90a4ccda426108a
Kirill <kirillpo2016@mail.ru>
2019-12-08 15:41:09 +0300

    аааы

--- include/Shop.h
+++ include/Shop.h
@@ -43,7 +43,7 @@ void draw_shop(Player &player)
 		str.setFont(font);
 		str.setPosition(shop.arr[i]->x_pos + 120*x, shop.arr[i]->y_pos + 142 * y);
 		std::string str1 = " Легко";
-		str.setString(str_to_wstr(str1));
+		str.setString(" Легко");
 		price.setPosition(shop.arr[i]->x_pos + 110*x , shop.arr[i]->y_pos + 130*y);
 		price.setScale(x, y);
 		window.draw(price);
