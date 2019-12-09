f9fc0e95dbbe954cdef9a3b40e0f6302f4b77f11
Frumka <Hekchanel@mail.ru>
2019-12-10 01:58:22 +0300

    Попробуем украсть еще больше

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 export GTEST_COLOR=1
 CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
@@ -8,4 +7,4 @@ CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='`pwd`/tools/polly/gcc-7-cxx17-pic.
 CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 cmake -H. -B_builds $CMAKE_OPTS
 cmake --build _builds
-cmake --build _builds --target student-life-tests
+cmake --build _builds --target test -- ARGS="--verbose"
